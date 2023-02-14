c36f1a2c488cad5aaa0bce8b9b749d7d8bae1fb2
iTzFabregas <fabricio-sampaio@usp.br>
2023-02-14 16:03:34 -0300

    feat: update navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Navbar from "./components/Navbar";
-import Aboutme from "./components/Aboutme"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
+//import Aboutme from "./components/Aboutme"
+//import Projects from "./components/Projects"
+//import Contact from "./components/Contact"
+//import Footer from "./components/Footer"
 
 import Aos from "aos";
 import 'aos/dist/aos.css'
@@ -12,13 +12,11 @@ function App() {
   Aos.init();
 
   return (
-    <div>
-      <Navbar />
-      <Aboutme />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
   );
 }
 
