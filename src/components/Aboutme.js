54318466ce22ee147554b4014ae7f4d925523513
iTzFabregas <fabricio-sampaio@usp.br>
2023-02-19 18:33:20 -0300

    feat: mobile support

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -12,19 +12,27 @@ export default function Aboutme() {
   }, [])
 
   return (
-    <div id='about' className='flex justify-center items-center gap-16 bg-neutral-900 text-white h-screen'>
-      <img src={logo} alt="capa" className='h-3/6 rounded-3xl shadow-my shadow-white' data-aos='fade-right'/>
-      <div className='text-left w-2/5 font-bold text-xl'>
-        <h1 className='text-gray-400 pb-1 font-normal'>Welcome to my site, I'm</h1>
-        <h3 className='text-2xl text-white pb-1'>Fabrício Sampaio</h3>
-        <h4 className='text-2xl text-gray-300 pb-1'>Computer scientist student</h4>
-        <p className='text-gray-500 font-normal pb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint totam voluptate dolorum, quidem eligendi harum veritatis velit ullam, accusamus obcaecati sapiente quo! Ducimus, recusandae quisquam? Aut quae quia sapiente accusamus esse rerum nesciunt eaque! Dolor exercitationem deleniti molestiae, expedita animi provident laudantium accusamus odio natus obcaecati voluptatem ex non amet.</p>
-        <ul className='flex gap-5'>
-          <li className='text-4xl hover:text-neutral-500'><a href="https://github.com/iTzFabregas"><FaGithub /></a></li>
-          <li className='text-4xl hover:text-neutral-500'><a href="https://www.linkedin.com/in/fabricio-sampaio"><FaLinkedinIn /></a></li>
-          <li className='text-4xl hover:text-neutral-500'><a href="mailto:fabricio-sampaio@usp.br"><FaEnvelope /></a></li>
-        </ul>
+    <div id='about' className='bg-neutral-900 text-white'>
+      <div className='flex md:flex-row flex-col justify-center items-center md:gap-16 gap-8 h-screen'>
+
+        <img src={logo} alt="capa" className='md:w-4/12 w-5/12 rounded-3xl'/>
+
+        <div className='md:text-left text-center md:w-2/5 w-4/5 font-bold text-xl'>
+
+          <h1 className='text-gray-400 pb-1 font-normal'>Welcome to my site, I'm</h1>
+          <h3 className='text-2xl text-white pb-1'>Fabrício Sampaio</h3>
+          <h4 className='text-2xl text-gray-300 pb-1'>Computer scientist student</h4>
+          <p className='text-gray-500 font-normal pb-4 text-base'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint totam voluptate dolorum, quidem eligendi harum veritatis velit ullam, accusamus obcaecati sapiente quo! Ducimus, recusandae quisquam? Aut quae quia sapiente accusamus esse rerum nesciunt eaque! Dolor exercitationem deleniti molestiae, expedita animi provident laudantium accusamus odio natus obcaecati voluptatem ex non amet.</p>
+
+          <ul className='flex gap-5 md:justify-start justify-center'>
+            <li className='text-4xl hover:text-neutral-500'><a href="https://github.com/iTzFabregas"><FaGithub /></a></li>
+            <li className='text-4xl hover:text-neutral-500'><a href="https://www.linkedin.com/in/fabricio-sampaio"><FaLinkedinIn /></a></li>
+            <li className='text-4xl hover:text-neutral-500'><a href="mailto:fabricio-sampaio@usp.br"><FaEnvelope /></a></li>
+          </ul>
+
+        </div>
       </div>
+
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,23 +4,22 @@ import logo from '../assets/sapo_ensaboado.png'
 export default function Navbar() {
   return (
     <nav className='fixed bg-neutral-900 bg-opacity-75 text-white w-screen'>
-      <div className='container flex flex-wrap justify-between items-center pt-4 mx-auto'>
-        <img src={logo} alt="logo" className='h-12 rounded-full ml-16' />
+      <div className='container flex justify-between items-center pt-4 pb-4 mx-auto'>
 
-        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
-          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-        </button>
+        <img src={logo} alt="logo" className='h-12 rounded-full md:ml-16 ml-8' />
 
-        <div id='navbar-default' class="hidden w-full md:block md:w-auto">
+        <div id='navbar-itens' class="hidden w-full md:block md:w-auto">
+
           <ul className='flex gap-16'>
             <li><a href="#about" className='hover:underline'>About</a></li>
             <li><a href="#projects" className='hover:underline'>Projects</a></li>
             <li><a href="#skills" className='hover:underline'>Skills</a></li>
             <li><a href="#contact" className='hover:underline'>Contact</a></li>
           </ul>
+
         </div>
-        <a href="https://github.com/itzfabregas" className='p-3 rounded-full bg-neutral-600 border-2 border-neutral-200 mr-16 hover:bg-neutral-700'>Portifolio</a>
+
+        <a href="https://github.com/itzfabregas" className='p-3 rounded-full bg-neutral-600 border-2 border-neutral-200 md:mr-16 mr-8 hover:bg-neutral-700'>Portifolio</a>
 
       </div>
     </nav>
