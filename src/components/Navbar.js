4b252957c3a17d7b475ced1d33c676208626b630
iTzFabregas <fabricio-sampaio@usp.br>
2023-02-19 01:02:38 -0300

    fix: typo in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,18 @@ import logo from '../assets/sapo_ensaboado.png'
 
 export default function Navbar() {
   return (
-  <div className='fixed z-50 bg-dark-500 w-full top-0 left-0 px-8 py-4 lg:px-20 xl:px-36'>
-      <div className="flex justify-between items-center text-white">
-          <img src={logo} className="App-logo w-10 rounded-full" alt="logo" />
-          <ul className="hidden md:flex"> 
-          <li className="p-4"><a href="#home" className="hover:underline">About</a></li>
-          <li className="p-4"><a href="#skills" className="hover:underline">Skills</a></li>
-          <li className="p-4"><a href="#honors" className="hover:underline">Honor & Awards</a></li>
-          <li className="p-4"><a href="#certs" className="hover:underline">Certfications</a></li>
-          </ul>
-          <a href="google.com" rel="noreferrer" target="_blank" className=" bg-slate-500 rounded-full px-4 py-1 border border-teal-800 border border-4">Resume</a>
-      </div>
-  </div>
+    
+    <div className='flex'>
+
+      <img src={logo} alt="EUUU" />
+      <ul>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+      <a href="https://github.com/itzfabregas">Portifólio</a>
+    </div>
+
   )
 }
