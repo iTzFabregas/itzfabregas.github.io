44d1f5746c4fae618f561ba9b00f01e1870af4ed
iTzFabregas <fabricio-sampaio@usp.br>
2025-03-29 21:26:16 -0300

    feat: add a smooth scroll in background

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import Navbar from "./components/Navbar";
 import Aboutme from "./components/Aboutme"
 import Projects from "./components/Projects"
+import { useEffect, useState } from "react";
 //import Contact from "./components/Contact"
 //import Footer from "./components/Footer"
 
 function App() {
 
-  return (
-    <>
-      <div>
-        <Navbar />
-        <Aboutme />
-        <Projects />
-      </div>
-    </>
-  );
+    const [offsetY, setOffsetY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => setOffsetY(window.scrollY);
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <>
+            <div className="relative h-[200vh]">
+
+                <div
+                    className="absolute inset-0 bg-cover bg-center"
+                    style={{
+                        backgroundImage: "url('./template.jpg')",
+                        transform: `translateY(${offsetY * 0.5}px)`, // Move em velocidade diferente
+                    }}
+                />
+
+                <Navbar />
+                <Aboutme />
+                <Projects />
+            </div>
+        </>
+    );
 }
 
 export default App;
